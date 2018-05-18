7418be9e3334b67470b400c421a2016e4f9d1b34
kkorona <high1uck@pusan.ac.kr>
2018-05-19 04:20:02 +0900

    Add files via upload

--- Distributor.py
+++ Distributor.py
@@ -1,14 +1,22 @@
 # -*- coding: utf-8 -*-
 import codecs
+import pymysql
 
 
 def add(aid, articleTitle, articleDate, articleTxt):
-    tList = ["sex", "major", "year", "name", "bus", "subway", "place", "class"]
-    l = []
+    #   conn = pymysql.connect(host='192.168.0.1', user='root',
+    #   password='high1uck', db='info', charset='utf8')
+
+    #   curs = conn.cursor()
+    #   sql = """insert into customer(aid,articleTitle,articleDate,sex,major,
+    #   year,name,bus,subway,place) values (%s, %s, %s, %s, %s,
+    #   %s, %s, %s, %s, %s)"""
+    tList = ["sex", "major", "year", "name", "bus", "subway", "place"]
+    l = (aid, articleTitle, articleDate)
     for target in tList:
         f = codecs.open(target + ".txt", 'r')
         lines = f.readlines()
-        appended = 0
+        element = None
         for line in lines:
             frags = line.split(":")
             cnt = 0
@@ -19,10 +27,9 @@ def add(aid, articleTitle, articleDate, articleTxt):
                 elif articleTxt.find(i) is not -1:
                     cnt = 1
             if cnt is not 0:
-                l.append([target, mainFrag])
-                appended = 1
-        if not appended and target == "sex":
-            l.append([target, "X"])
+                element = mainFrag
+                break
+        l += (element,)
     print(l)
 
 if __name__ == "__main__":
--- crawler.py
+++ crawler.py
@@ -7,33 +7,47 @@ import requests
 def main():
     cookies = {'guidance_sun_d41d8cd98f00b204e9800998ecf8427e': 'true'}
     ff = codecs.open("getArticle.txt", 'w', 'utf-8')
+    ff.close()
+    ff = codecs.open("getArticle.txt", 'a', 'utf-8')
     result = ""
-    for i in range(1, 10, 1):
+    for i in range(1, 100, 1):
         url = "http://mypnu.net/index.php?mid=sun&page="+str(i)
         print(url)
         req = requests.get(url, cookies=cookies)
         html = req.text
         bs = BeautifulSoup(html, 'html.parser')
         l = bs.find_all('a', 'hx')
-        for m in l:
+        v = bs.find_all('td', 'm_no')
+        vv = []
+        for view in v:
+            vv.append(view.get_text())
+        vv[0:4] = []
+        for m, view in zip(l, vv):
             articleTitle = m.get_text().strip()
             articleURL = m.get('href')
             aidIndex = articleURL.find('document_srl=') + 13
             aid = articleURL[aidIndex:]
-
-            print(articleTitle)
-            print(articleURL)
-            print(aid)
 
             aReq = requests.get(articleURL, cookies=cookies)
             html2 = aReq.text
             aBs = BeautifulSoup(html2, 'html.parser')
             articleDate = aBs.find('span', 'date m_no')
             ll = aBs.find('div', 'document_' + aid + '_0 xe_content')
-            result += ("- " + aid + " -\n" + articleTitle + "\n"
-                       "\n" + mm.get_text() + "\n--------------"
-                       "----------------\n\n")
-    ff.write(result)
+            articleView = view
+            if articleDate is None:
+                continue
+            articleDate = articleDate.get_text()
+            articleTxt = ll.get_text()
+            articleView = view
+            print(articleTitle)
+            print(articleDate)
+            print(aid)
+
+            result = ("- " + aid + " -\n" + articleTitle + "\n" +
+                      articleDate + "\n" + articleView + "\n" +
+                      articleTxt + "\n--------------" +
+                      "----------------\n")
+            ff.write(result)
     ff.close()
 
 if __name__ == "__main__":
