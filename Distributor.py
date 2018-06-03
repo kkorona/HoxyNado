ccfda82e369609e5242e0961e5e41db1757f346b
kkorona <high1uck@pusan.ac.kr>
2018-06-03 16:02:21 +0900

    Add files via upload

--- Distributor.py
+++ Distributor.py
@@ -3,6 +3,10 @@ import codecs
 import pymysql
 
 
+def dump(a, b):
+    print(str(a) + " \"" + str(b) + "\"")
+
+
 def add(aid, articleTitle, articleDate, articleTxt):
     #   conn = pymysql.connect(host='192.168.0.1', user='root',
     #   password='high1uck', db='info', charset='utf8')
@@ -22,6 +26,7 @@ def add(aid, articleTitle, articleDate, articleTxt):
             cnt = 0
             mainFrag = frags[0]
             for i in frags:
+                if i is '\n': continue
                 if articleTitle.find(i) is not -1:
                     cnt = 1
                 elif articleTxt.find(i) is not -1:
@@ -33,5 +38,7 @@ def add(aid, articleTitle, articleDate, articleTxt):
     print(l)
 
 if __name__ == "__main__":
-    add("111", "16 경영 ㄱㄴㄷ", "0001.01.01 00:00",
-        "남친 있나요?")
+    add("2211659.", "오늘 열시반 미학 발표하신 분", "0001.01.01 00:00", "인"
+        "상주의 발표하신 정외과 남자분 !\n 목소리 너무 좋으셨어요ㅠㅠ"
+        "asmr 해주셨으면 하는 목소리...\n 다시 목소리 들을 일은 없겠지"
+        "만 ㅠ 발표하시는동안 경청했습니다 ㅎㅎㅎ 발표 수고하셨어요 !")
