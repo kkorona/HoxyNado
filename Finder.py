3240ecefc9352fad597324d9cb472c4840eb2513
Ash <37513297+Ash-JSW@users.noreply.github.com>
2018-06-22 22:11:26 +0900

    180622 // 22:11

--- Finder.py
+++ Finder.py
@@ -5,40 +5,40 @@ import pathlib
 from pprint import pprint
 
 def data_to_json(data) :
-    if type(data) is str : # 타占쏙옙占쏙옙 占쏙옙占쌘울옙占싱띰옙占?
-        return '"' + data + '"' # 占쏙옙占쌘울옙占쏙옙 "占쏙옙 占쏙옙占쏙옙占쌍곤옙
-    elif type(data) is list : # 타占쏙옙占쏙옙 占쏙옙占쏙옙트占쏙옙占?
-        return list_to_json(data, data_to_json) # 占쌉쇽옙 호占쏙옙
-    elif type(data) is int or type(data) is float : # 타占쏙옙占쏙옙 占쏙옙占쌘띰옙占?
-        return data.__str__() # 占쌓댐옙占? 占쏙옙환
-    elif type(data) is dict : # 타占쏙옙占쏙옙 dict占쏙옙占?
-        return dict_to_json(data, data_to_json) # 占쌉쇽옙 호占쏙옙
+    if type(data) is str : 
+        return '"' + data + '"' 
+    elif type(data) is list : 
+        return list_to_json(data, data_to_json) 
+    elif type(data) is int or type(data) is float : 
+        return data.__str__() 
+    elif type(data) is dict : 
+        return dict_to_json(data, data_to_json) 
     else :
         return '""'
 
 def list_to_json(list, func):
-    out_str = "[" # [(占쏙옙占싫?)占쏙옙 占쏙옙占쏙옙
+    out_str = "[" 
     for val in list:
         out_str += func(val)
-        out_str += ", " # ,(占쏙옙표)占쏙옙 占쏙옙占쏙옙占싶몌옙 占쏙옙占쏙옙
+        out_str += ", "
 
     if len(out_str) > 2:
         out_str = out_str[:-2]
 
-    out_str += "]" # ](占쏙옙占싫?)占쏙옙 占쌥는댐옙
+    out_str += "]" 
     return out_str
 
 def dict_to_json(dict, func) :
-    out_str = "{" # {(占쌩곤옙호)占쏙옙 占쏙옙占쏙옙
+    out_str = "{"
     for key in dict.keys() :
-        out_str += ('"' + key.__str__() + '"') # 키 占쏙옙占쏙옙 "(큰 占쏙옙占쏙옙표)占쏙옙 占쏙옙占쏙옙占?
-        out_str += ": " # :(占쌥뤄옙)占쏙옙占쏙옙 Key占쏙옙 Value占쏙옙 占싻몌옙
+        out_str += ('"' + key.__str__() + '"') 
+        out_str += ": " 
         out_str += func(dict[key])
-        out_str += ", " # ,(占쏙옙표)占쏙옙 占쌍곤옙 占쏙옙占쏙옙 占싻몌옙
+        out_str += ", " 
     if len(out_str) > 2:
         out_str = out_str[:-2]
 
-    out_str += "}" # }(占쌩곤옙호)占쏙옙 占쌥는댐옙
+    out_str += "}" 
     return out_str
 
 def main():
