8417d3cca513b9c8224fd1329ab65bc3124f1651
Ash <37513297+Ash-JSW@users.noreply.github.com>
2018-06-23 03:51:45 +0900

    180623 // 03:50 / 주석 1차 완료

--- crawler.py
+++ crawler.py
@@ -18,8 +18,8 @@ def main():
         req = requests.get(url, cookies=cookies) # 해당 url에서 html 코드를 가져온다
         html = req.text # 가져온 html코드를 text로 변경
         bs = BeautifulSoup(html, 'html.parser') # 
-        l = bs.find_all('a', 'hx') # (약간 헷갈림) 게시판에서 글의 제목을 포함하고 있는 HTML 태그는 <a class = "hx"> 이다.
-        v = bs.find_all('td', 'm_no') # (헷갈림) 게시판에서 글의 조회수를 포함하고 있는 HTML 태그는 <td class = "m_no"> 이다.
+        l = bs.find_all('a', 'hx') # 게시판에서 글의 제목을 포함하고 있는 HTML 태그는 <a class = "hx"> 이다.
+        v = bs.find_all('td', 'm_no') # 게시판에서 글의 조회수를 포함하고 있는 HTML 태그는 <td class = "m_no"> 이다.
         vv = []
         for view in v:
             vv.append(view.get_text())
@@ -41,14 +41,15 @@ def main():
                 continue
             articleDate = articleDate.get_text() # 게시물 작성 날짜 및 시간
             articleTxt = ll.get_text() # 게시물의 글 내용
-            articleView = view # 5줄위에 똑같은 코드 있음
+            articleView = view 
 
             result = ("- " + aid + " -\n" + articleTitle + "\n" +   
                       articleDate + "\n" + articleView + "\n" +     
                       articleTxt + "\n--------------" +                                     
                       "----------------\n") # 크롤링한 데이터들을 result로 묶었다.
             ff.write(result) # getArticle에 result를 저장한다. 
-            Distributor.add(aid, articleTitle, articleDate, articleTxt) # getArticle.txt에 저장한 크롤링한 데이터를 분류처리 모듈로 보낸다.
+            Distributor.add(aid, articleTitle, articleDate, articleTxt)
+            # getArticle.txt에 저장한 크롤링한 데이터를 분류처리 모듈로 보낸다.
     ff.close() # 사용한 파일을 닫아 준다.
 
 if __name__ == "__main__":
